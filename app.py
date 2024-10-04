bf1e0a3070106745aa438c5d54370bbd8e52e639
OsamahBinSALMAN <51197969+OsamahBinSALMAN@users.noreply.github.com>
2024-10-04 15:21:50 +0300

    app.py

--- app.py
+++ app.py
@@ -8,23 +8,17 @@ app = Flask(__name__)
 with open('bagging_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Tahmin işlemi için AJAX isteğini kabul eden route
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # AJAX ile gelen veriyi al
-        data = request.json['data']
-        # Veriyi işleyip numpy array'e dönüştür
-        input_array = np.array([float(i) for i in data.split(',')]).reshape(1, -1)
-        
-        # Modelden tahmin sonucu al
-        prediction = model.predict(input_array)[0]
-        
-        # JSON formatında sonucu döndür
-        return jsonify({'prediction': prediction})
+        data = request.json['data']  # AJAX ile gelen veriyi al
+        input_array = np.array([float(i) for i in data.split(',')]).reshape(1, -1)  # Veriyi işle
+        prediction = model.predict(input_array)[0]  # Modelden tahmin sonucu al
+        return jsonify({'prediction': prediction})  # JSON formatında sonucu döndür
     
     except Exception as e:
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
+    # Geliştirme modunda direkt Flask'ın dahili sunucusunu çalıştır
     app.run(debug=True)
