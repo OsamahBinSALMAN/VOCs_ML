32a095b6fd02acdefe63a39c654a39823b834cb1
OsamahBinSALMAN <51197969+OsamahBinSALMAN@users.noreply.github.com>
2024-10-04 15:46:14 +0300

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # CORS kütüphanesini ekledik
 import pickle
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)  # Tüm yollar ve kökenler için CORS'u aktif ediyoruz
 
 # Kaydedilmiş modeli yükle
 with open('bagging_model.pkl', 'rb') as file:
@@ -20,5 +22,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    # Geliştirme modunda direkt Flask'ın dahili sunucusunu çalıştır
     app.run(debug=True)
+
